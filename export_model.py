d8a0b57ab9ca157f7a1536d8b42df2b9c064dbf0
dnth <dickson.neoh@gmail.com>
2024-10-23 10:21:37 +0800

    update

--- export_model.py
+++ export_model.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load the PyTorch model
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8s.pt")
 
 # Export as an NCNN format
 model.export(format="ncnn", imgsz=320, half=True)
